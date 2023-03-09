24b769acdfcc9139451cb2a7254769853f1489b2
marianitadileo <mdileo36@gmail.com>
2023-03-09 18:14:49 +0100

    add milestone 1

--- js/script.js
+++ js/script.js
@@ -5,27 +5,33 @@ sendBtn.addEventListener("click", function() {
     const userKm = inputUserKm.value; 
     const userAge = inputUserAge.value;
     console.log(userKm, userAge);
+
+    const priceKm = 0.21;
+
+    const priceJourney = userKm * priceKm;
+    console.log(priceJourney);
+
+    let discountUnder = 0; 
+    let discountOver = 0;
+    let reducedPrice = 0; 
+    let finalPrice;
+
+    if(userAge < 18) {
+        discountUnder = 0.2;
+        reducedPrice = priceJourney * discountUnder;
+        finalPrice = priceJourney - reducedPrice;
+        console.log(finalPrice);
+    } else if (userAge > 65) {
+        discountOver = 0.4;
+        reducedPrice = priceJourney - discountOver;
+        console.log(reducedPrice);
+    } else {
+        console.log(priceJourney);
+    }
+
+    inputUserKm.value = "";
+    inputUserAge.value = "";
 })
 
-const priceKm = 0.21;
-const userKm = inputUserKm.value;
 
-const priceJourney = userKm * priceKm;
-console.log(priceJourney);
-
-let discountUnder = 0; 
-let discountOver = 0;
-let reducedPrice = 0; 
-
-if(userAge < 18) {
-    discountUnder = 0.02;
-    reducedPrice = priceJourney - discountUnder; 
-    console.log(reducedPrice);
-} else if (userAge > 65) {
-    discountOver = 0.4;
-    reducedPrice = priceJourney - discountOver;
-    console.log(reducedPrice);
-} else {
-    console.log(priceJourney);
-}
 
